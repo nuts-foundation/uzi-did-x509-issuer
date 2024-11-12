9a22269f21336e3eaddd562d7a513590d15abc6b
Roland Groen <roland@headease.nl>
2024-11-12 16:52:23 +0100

    BUGFIX: Add buffered output for JWT printing (#15)
    * Add buffered output for JWT printing
    
    Replaced println with a buffered writer when outputting the JWT to ensure the output is properly flushed. Introduced a new helper function `printLineAndFlush` to handle the buffered writing and flushing process.
    
    * Handle errors from printLineAndFlush function
    
    Updated the printLineAndFlush function to return an error instead of terminating the program. Modified the main function to handle the error appropriately by logging the error and exiting when printLineAndFlush fails.
    
    * Simplified parsing
    
    ---------
    
    Co-authored-by: Rein Krul <info@reinkrul.nl>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/alecthomas/kong"
 	"github.com/nuts-foundation/uzi-did-x509-issuer/uzi_vc_issuer"
@@ -48,7 +49,11 @@ func main() {
 			fmt.Println(err)
 			os.Exit(-1)
 		}
-		println(jwt)
+		err = printLineAndFlush(jwt)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(-1)
+		}
 	case "test-cert <uzi> <ura> <agb>", "test-cert <uzi> <ura> <agb> <subject_did>":
 		// Format is 2.16.528.1.1007.99.2110-1-900030787-S-90000380-00.000-11223344
 		// <OID CA>-<versie-nr>-<UZI-nr>-<pastype>-<Abonnee-nr>-<rol>-<AGB-code>
@@ -93,11 +98,27 @@ func main() {
 			fmt.Println(err)
 			os.Exit(-1)
 		}
-		println(jwt)
+		err = printLineAndFlush(jwt)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(-1)
+		}
 	default:
 		fmt.Println("Unknown command")
 		os.Exit(-1)
 	}
+}
+
+// printLineAndFlush writes a JWT (JSON Web Token) to the standard output and flushes the buffered writer.
+func printLineAndFlush(jwt string) error {
+	f := bufio.NewWriter(os.Stdout)
+	// Make sure to flush
+	defer func(f *bufio.Writer) {
+		_ = f.Flush()
+	}(f)
+	// Write the JWT
+	_, err := f.WriteString(jwt + "\n")
+	return err
 }
 
 func issueVc(vc VC) (string, error) {
